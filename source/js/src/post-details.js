ac21ebef9c4d97542b3e19dd060ed497952ebdcf
Tsuki <i@sukitsuki.com>
2017-11-06 02:55:00 +0800

    make post not enable sidebar

--- source/js/src/post-details.js
+++ source/js/src/post-details.js
@@ -85,8 +85,7 @@ function initToc() {
 
   // Expand sidebar on post detail page by default, when post has a toc.
   var $tocContent = $('.post-toc-content');
-  var isSidebarCouldDisplay = CONFIG.sidebar.display === 'post' ||
-    CONFIG.sidebar.display === 'always';
+  var isSidebarCouldDisplay = CONFIG.sidebar.display === 'always';
   var hasTOC = $tocContent.length > 0 && $tocContent.html().trim().length > 0;
   if (isSidebarCouldDisplay && hasTOC) {
     CONFIG.motion.enable ?
